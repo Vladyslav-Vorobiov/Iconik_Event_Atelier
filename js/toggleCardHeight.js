21a31b2618bb8ca23659a9ec37f47c45067c8e85
VladITGod <vorobiov.v90@gmail.com>
2023-06-04 00:45:34 +0300

    developing more

---
+++ js/toggleCardHeight.js
@@ -0,0 +1,16 @@
+const sectionMembershipEl = document.querySelector(".membership");
+
+sectionMembershipEl.addEventListener("click", grow);
+
+function grow(e) {
+  e.stopPropagation();
+  if (e.target.classList.contains("note-card__btn-all")) {
+    e.target.innerText =
+      e.target.innerText.toLowerCase().trim() === "view all features"
+        ? "hide all features"
+        : "view all features";
+
+    e.target.previousElementSibling.classList.toggle("toggle-card-height");
+  }
+  return;
+}
